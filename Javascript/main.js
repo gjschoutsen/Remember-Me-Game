0547818d08e0d435d928ba44a72c23b4b5bd8bf5
Gertjan Schoutsen <gjs@Gertjans-MacBook-Pro.local>
2022-01-25 12:59:05 +0100

    added compare player patern with computer patern and win or lose function

--- Javascript/main.js
+++ Javascript/main.js
@@ -8,22 +8,17 @@ class Game {
     this.flashCount = 3;
     this.flashSpeed = 600;
     this.flashOrder = [];
-    this.playerPatern = []
-    this.computerPatern = []
-    
+    this.playerPatern = [];
+    this.playerIsDone = false;
   }
 
   startGame() {
-    this.addEventListener();
-  }
+    this.tileStart.addEventListener("click", (e) => {
+      this.patternGenerator();
 
-  winGame(){
-    if (this.playerPatern === this.computerPatern){
-    alert("YOU WON, WELL DONE!!")
-  } else {
-      alert("OH NO, YOU LOST!")
+      console.log("clicked tile START");
+    });;
   }
-}
 
   flashTile(tileNr, removeClass, addClass) {
     tileNr.classList.remove(removeClass);
@@ -67,42 +62,66 @@ class Game {
 
       if (count > this.flashOrder.length) {
         clearInterval(flashTimer);
+        this.playerClick()
       }
       count++;
     }, this.flashSpeed);
-    console.log(this.playerPatern)
+    
+    console.log(this.playerPatern);
   }
 
-  addEventListener() {
-    this.tileStart.addEventListener("click", (e) => {
-        this.patternGenerator();
-
-      console.log("clicked tile START");
-    });
+  playerClick() {
+   
     this.tile1.addEventListener("click", (e) => {
-        this.playerPatern.push(1)
-        this.playerPatern.push(0)
-
+      this.playerPatern.push(1);
+      this.playerPatern.push(0);
+      this.winLoseStatus()
+      
       console.log("clicked tile 1");
     });
     this.tile2.addEventListener("click", (e) => {
-        this.playerPatern.push(2)
-        this.playerPatern.push(0)
-
+      this.playerPatern.push(2);
+      this.playerPatern.push(0);
+      this.winLoseStatus()
       console.log("clicked tile 2");
     });
     this.tile3.addEventListener("click", (e) => {
-        this.playerPatern.push(3)
-        this.playerPatern.push(0)
-
+      this.playerPatern.push(3);
+      this.playerPatern.push(0);
+      this.winLoseStatus()
       console.log("clicked tile 3");
     });
     this.tile4.addEventListener("click", (e) => {
-        this.playerPatern.push(4)
-        this.playerPatern.push(0)
-
+      this.playerPatern.push(4);
+      this.playerPatern.push(0);
+      this.winLoseStatus()
       console.log("clicked tile 4");
     });
+
+  }
+
+  startWinLose(){
+    if (this.computerIsDone){
+      this.winLoseStatus();
+    }
+  }
+
+  winLoseStatus() {
+    if (
+      this.flashOrder.length === this.playerPatern.length
+    ) {
+      this.checkWinLose();
+    }
+  }
+
+  checkWinLose() {
+    if (JSON.stringify(this.playerPatern) === JSON.stringify(this.flashOrder)) {
+      alert("YOU WON, WELL DONE!!");
+      location.reload();
+    } else {
+      alert("OH NO, YOU LOST!");
+      location.reload;
+    }
   }
 }
 
